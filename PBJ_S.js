eb3f3ec4c87f140ed96fd29525e4f6562dc8da97
Kyler Love <tue94528@temple.edu>
2016-03-11 13:23:30 -0500

    PBJ_S update
    added functions: addition, subtraction, fuck_you

--- PBJ_S.js
+++ PBJ_S.js
@@ -1,10 +1,59 @@
 
-//http://checkman.io/blog/creating-a-javascript-library/
 
-var PBJS = {
-    name: "PBJ_S",
-    greet: function(){
+(function (window) {
 
-    alert("Hello from the " + PBJS.name + " library.");
-    }
+'use strict';
+
+
+function define_PBJS(){
+
+  var PBJS = {};
+
+  ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>beginning of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+	// About
+PBJS.ver = function(){
+      console.log("Version: 0.1\n Author: 'Kyler Love'\n Updated: '11 March 2016'");
 };
+PBJS.greet = function(){
+      console.log("Welcome to Peanut Butter JavaScript... or Peanut Butter Jelly Sandwich... Both are delicious");
+};
+
+//adds values.  pass in the 'id' of the two inputs
+PBJS.add = function(a, b){
+  var A = document.getElementById(a).value;
+  var B = document.getElementById(b).value;
+  console.log(parseInt(A) + parseInt(B));
+    return A + B;
+};
+
+//subtracts values.  pass in the 'id' of the two inputs
+PBJS.subtract = function(a, b){
+  var A = document.getElementById(a).value;
+  var B = document.getElementById(b).value;
+  console.log(parseInt(A) - parseInt(B));
+    return A - B;
+};
+
+PBJS.fuckyou = function(){
+  var fucks = document.querySelectorAll("h1, h2, h3, h4, h5, h6, button, input, p");
+  console.log(fucks.innerHTML);
+  for(var i = 0; i < fucks.length; i++){
+   fucks[i].innerHTML = 'FUCK YOU';
+}
+
+};
+
+
+///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>End of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+return PBJS;
+
+}
+
+if(typeof(PBJS) === 'undefined'){
+
+  window.PBJS = define_PBJS();
+}
+
+}) (window);
