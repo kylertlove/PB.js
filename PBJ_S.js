cc61f1b9d2a58f3a98b8adef94bf838cf9436e27
kylertlove <tue94528@temple.edu>
2016-03-12 12:36:57 -0500

    clock maker

--- PBJ_S.js
+++ PBJ_S.js
@@ -1,72 +1,90 @@
+(function(window) {
+
+  'use strict';
 
 
-(function (window) {
+  function define_PBJS() {
 
-'use strict';
+    var PBJS = {};
 
+    ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>beginning of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-function define_PBJS(){
-
-  var PBJS = {};
-
-  ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>beginning of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-	// About
-PBJS.ver = function(){
-      console.log("Version: 0.1\n Author: 'Kyler Love'\n Updated: '11 March 2016'");
-};
-  //greet
-PBJS.greet = function(){
+    // About
+    PBJS.ver = function() {
+      console.log(" Version: 0.1\n Author:  'Kyler Love'\n Updated: '11 March 2016'");
+    };
+    //greet
+    PBJS.greet = function() {
       console.log("Welcome to Peanut Butter JavaScript... or Peanut Butter Jelly Sandwich... Both are delicious");
-};
+    };
 
-//adds values.  pass in the 'id' of the two inputs
-PBJS.add = function(a, b){
-  /* jshint ignore:start */
-  let A = document.getElementById(a).value;
-  let B = document.getElementById(b).value;
-  /* jshint ignore:end */
-  console.log(parseInt(A) + parseInt(B));
-    return A + B;
-};
+    //adds values.  pass in the 'id' of the two inputs
+    PBJS.add = function(a, b) {
+      /* jshint ignore:start */
+      let A = document.getElementById(a).value;
+      let B = document.getElementById(b).value;
+      /* jshint ignore:end */
+      console.log(parseInt(A) + parseInt(B));
+      return A + B;
+    };
 
-//subtracts values.  pass in the 'id' of the two inputs
-PBJS.subtract = function(a, b){
-  /* jshint ignore:start */
-  let A = document.getElementById(a).value;
-  let B = document.getElementById(b).value;
-  /* jshint ignore:end */
-  console.log(parseInt(A) - parseInt(B));
-    return A - B;
-};
+    //subtracts values.  pass in the 'id' of the two inputs
+    PBJS.subtract = function(a, b) {
+      /* jshint ignore:start */
+      let A = document.getElementById(a).value;
+      let B = document.getElementById(b).value;
+      /* jshint ignore:end */
+      console.log(parseInt(A) - parseInt(B));
+      return A - B;
+    };
 
-//fuck it all
-PBJS.fuckitall = function(){
-  /* jshint ignore:start */
-  let fucks = document.querySelectorAll("h1, h2, h3, h4, h5, h6, button, input, p");
-  /* jshint ignore:end */
-  console.log(fucks.innerHTML);
-  for(var i = 0; i < fucks.length; i++){
-   fucks[i].innerHTML = 'FUCK IT ALL';
-}
-};
+    //fuck it all
+    PBJS.fuckitall = function() {
+      /* jshint ignore:start */
+      let fucks = document.querySelectorAll("h1, h2, h3, h4, h5, h6, button, input, p, a");
+      /* jshint ignore:end */
+      console.log(fucks.innerHTML);
+      for (var i = 0; i < fucks.length; i++) {
 
-PBJS.TraceyHarrisonManifesto = function(){
-  var NothingMatters = "<div style='Font-size: 200px;'>NOTHING MATTERS<div>";
-  var bod = document.querySelector('body');
-  bod.innerHTML = NothingMatters;
-};
+        //input fields change placeholder
+        if (fucks[i].tagName.toLowerCase() === 'input') {
+          fucks[i].placeholder = 'FUCK IT ALL';
+        }
+
+        //testing
+        console.log(fucks[i].tagName.toLowerCase());
+        console.log(fucks[i].innerHTML);
+
+        //change dom
+        fucks[i].innerHTML = 'FUCK IT ALL';
+      }
+
+    };
+
+    //Tracey's take on current events
+    PBJS.The_Tracey_Harrison_Manifesto = function() {
+      var NothingMatters = "<div style='Font-size: 200px;'>NOTHING MATTERS<div>";
+      var bod = document.querySelector('body');
+      bod.innerHTML = NothingMatters;
+    };
+
+    //turn the innerHTML of any element into a clock
+    PBJS.AddClock = function(elem) {
+      var tag = document.getElementById(elem);
+      var now = new Date();
+      tag.innerHTML = now.toLocaleTimeString();
+    };
 
 
-///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>End of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>End of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-return PBJS;
+    return PBJS;
 
-}
+  }
 
-if(typeof(PBJS) === 'undefined'){
+  if (typeof(PBJS) === 'undefined') {
 
-  window.PBJS = define_PBJS();
-}
+    window.PBJS = define_PBJS();
+  }
 
-}) (window);
+})(window);
